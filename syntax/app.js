dd43489170efa99ac037705aad506205c80c55b1
blueplanet <erguolinge@gmail.com>
2017-01-24 06:30:44 +0900

    watch

--- syntax/app.js
+++ syntax/app.js
@@ -9,3 +9,38 @@ var vm = new Vue({
     }
   }
 })
+
+var wvm = new Vue({
+  el: '#watch-example',
+  data: {
+    question: '',
+    answer: 'I cannot gifve you an answer until you ask a question!'
+  },
+  watch: {
+    question: function(newQuestion) {
+      this.answer = 'Waiting for you to stop typing...'
+      this.getAnswer()
+    }
+  },
+  methods: {
+    getAnswer: _.debounce(
+      function() {
+        var vm = this
+        if (this.question.indexOf('?') === -1) {
+          vm.answer = 'Questions usually contain a question mark. ;-)'
+          return
+        }
+
+        vm.answer = 'Thinking...'
+        axios.get('https://yesno.wtf/api')
+          .then(function(response) {
+            vm.answer = _.capitalize(response.data.answer)
+          })
+          .catch(function(error) {
+            vm.answer = 'Error! cound not reach the API: ' + error
+          })
+      },
+      500
+    )
+  }
+})
