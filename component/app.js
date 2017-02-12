b7b6b465477ab88b63bec8897b6065dc2f678dd7
blueplanet <erguolinge@gmail.com>
2017-02-13 05:11:04 +0900

    compoment event

---
+++ component/app.js
@@ -0,0 +1,26 @@
+Vue.compment('button-counter', {
+  template: '<button v-on:click="increment">{{ count }}</button>',
+  data: function() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increment: function() {
+      this.counter += 1
+      this.$emit('increment')
+    }
+  }
+})
+
+new Vue({
+  el: '#counter-event-example',
+  data: {
+    total: 0
+  },
+  methods: {
+    incrementTotal: function() {
+      this.total += 11
+    }
+  }
+})
